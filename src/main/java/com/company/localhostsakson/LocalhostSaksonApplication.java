030c594cdcd91948e5273758fc3fc0e0654923e4
abdumomin <shamshiddinovabdumomin2003@gmail.com>
2024-02-05 09:38:27 +0500

    Initial test commit

--- src/main/java/com/company/localhostsakson/LocalhostSaksonApplication.java
+++ src/main/java/com/company/localhostsakson/LocalhostSaksonApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LocalhostSaksonApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(LocalhostSaksonApplication.class, args);
     }
 
